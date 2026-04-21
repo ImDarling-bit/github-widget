af3028efbdb004ec611791a6b3aad8cea74b983a
Noah | _ImDarling_ <101842721+ImDarling-bit@users.noreply.github.com>
2026-04-21 15:00:19 +0200

    Update download URLs to use refs/heads/main

--- install.ps1
+++ install.ps1
@@ -4,9 +4,9 @@ New-Item -ItemType Directory -Force -Path "css", "js"
 Write-Host "Téléchargement des fichiers du widget..." -ForegroundColor Cyan
 
 # Téléchargement du CSS
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ImDarling-bit/github-widget/main/widget/github-widget.css" -OutFile "css/github-widget.css"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ImDarling-bit/github-widget/refs/heads/main/widget/github-widget.css" -OutFile "css/github-widget.css"
 
 # Téléchargement du JS
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ImDarling-bit/github-widget/main/widget/github-widget.js" -OutFile "js/github-widget.js"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ImDarling-bit/github-widget/refs/heads/main/widget/github-widget.js" -OutFile "js/github-widget.js"
 
 Write-Host "Installation terminée avec succès !" -ForegroundColor Green
